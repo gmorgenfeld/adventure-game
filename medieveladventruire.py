1744b3fe1238b9b664f6738632f53e218b1aa70c
gmorgenfeld <50252330+gmorgenfeld@users.noreply.github.com>
2019-05-07 21:01:01 -0700

    Update medieveladventruire.py

--- medieveladventruire.py
+++ medieveladventruire.py
@@ -5,7 +5,7 @@ from type import *
 user=input('Please enter you name: ')
 type('Welcome ' + user + ' to the dangerously, amazing world of medieval land! I am your guide, ernesto! You have been challenged to become the greatest explorer and fighter in all the land!')
 sleep(1)
-skills
+#skills
 type('You have special skills. Strength, speed, perception, and charisma. You only have 20 points, so use wisely!')
 def game() :
     
@@ -57,8 +57,10 @@ def game() :
         else:
             exit()
              
-    
+
+        
     if strength >= 8:
+        
         type('You are strong enough to choose a special power!')
         type('(One): Determined: +' + str(strength) + ' extra attack per level up')
         type('(Two): Rage: *3 attack on choice of one attack in battle, once per battle')
@@ -72,11 +74,28 @@ def game() :
 
         if strengthpower == 'three':
             type('You have selected: Rough Beginnings: +25 attack right from start')
+        else:
+            type('Please choose one of the options')
+                
+        
+    if speed >= 8:
+        type('You are fast enough to choose a special power!')
+        type('(One): Runner: Allows you to run from any battle with an enemy 5 levels away from you. (No bosses)')
+        type('(Two): Agile: Allows you to attack twice, once per battle')
+        type('(Three): Not the strongest: -2 strength, +2 speed')
+        speedpower = input('Which power? (One, Two, Three): ').lower()
+        if speedpower == 'one':
+            type('You chose Runner: Allows you to run from any battle with an enemy 5 levels away from you. (No bosses)')
 
+        if speedpower == 'two':
+            type('You chose: Agile: Allows you to attack twice, once per battle')
 
-
+        if speedpower == 'three':
+            type('You chose: Not the strongest: -2 strength, +2 speed')
+            
+            
 
 
 game()
+    
             
-
