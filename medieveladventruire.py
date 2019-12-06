180bf59d2a0783b7a1c0927f10b0e6b0159dc55a
gmorgenfeld <50252330+gmorgenfeld@users.noreply.github.com>
2019-12-05 19:36:56 -0800

    Update medieveladventruire.py

--- medieveladventruire.py
+++ medieveladventruire.py
@@ -17,11 +17,37 @@ ears = False
 seecoming = False
 looter = False
 #intro
-user=input('Please enter you name: ')
-#type('Welcome ' + user + ' to the dangerously, amazing world of medieval land! I am your guide, ernesto! You have been challenged to become the greatest explorer and fighter in all the land!')
+
+type('You there!')
+sleep(1)
+type('Yes you! What is your name!')
+name=input('What is your name?: ')
+
+
+type (name + '!')
+type ('By the order of the King of Ceteron, I order you to join our Great Army in the fight against the ruthless, pillaging Yothil Dynasty!')
+sleep(2)
+type('The man takes you to a caravan. You get in, not realizeing that that would be the last time you would see peace for a while')
+sleep(5)
+type('Two weeks later')
+sleep(1)
+type('You\'re on the frontlines, all you can see is smoke and fire. People around you fall to their knees. Chests filled with the Dynastys deadly arrows.')
+sleep(1)
+type('All you can think about is where you could be besides here, then someone yells to get in the trenches.')
+type('You do so, as it is a safer place')
+sleep(1)
+type('Then a boulder, one from the catapults, rolls over the trench, crushing people and collapsing the dirt around you. You barely get the chance to drop down and you are saved by inches from the boulder.')
+sleep(1)
+type('Time passes, nobody comes to help. You realize that you are going to have to get off the battlefield alone.')
+sleep(1)
+type('You slowly slip out from under the boulder, careful not to collapse anything else. You step through puddles of blood and flesh. Finally you make it out. It is much calmer now, but still deadly')
+sleep(1)
+type('You see a forest through the smoke and you sprint for it, trying to stay low. The arrows whiz past your ear and one hits your ankle. You dive into the brush and sit for a minute')
+sleep(1)
+type('Your character is your character and needs to be customized. You have a set of skills. Strength, speed, perception, and charisma. If you have 8 or more points in a catagory, you get to choose a power! You only have 20 points, so choose wisely!')
 sleep(1)
 #skills
-#type('You have special skills. Strength, speed, perception, and charisma. You only have 20 points, so choose wisely!')
+
 def game() :
     
     points = 20
@@ -72,7 +98,7 @@ def game() :
         else:
             exit()
              
-
+#super stats
         
     if strength >= 8:
         done = False
@@ -95,7 +121,7 @@ def game() :
             elif strengthpower == 'three':
                 type('You have selected: Rough Beginnings: +25 attack right from start')
                 attack =+ 25
-                print (attack)
+                type('You now have ' + str(attack) + ' attack!')
                 done = True
 
             else:
@@ -193,32 +219,46 @@ def game() :
                 type('Please choose one of the options')
                 done = True
 
-    tutorial = True
-    #type('Ok! That\'s quite the skillset you\'ve got there! That will take you very far in this world.')
-    #type('Now, you need to know how to use those skills in order for them to be useful, however you may already know!')
-    tutorial = input('Take the tutorial?(y/n): ').lower
-    if tutorial == 'y':
-        tutorial = True
-    elif tutorial == 'n':
-        tutorial = False
-    else:
-        tutorial = True
-
-        while tutorial == False:
-            exit()
-            
-
-        while tutorial == True:
-            print('yeet')
-
-
-        
-
-            
-        
-        
-        
     
+
+
+    makesure=input('This will be your character for the game. Is this how you want it? (y/n):  ')
+    if makesure == 'n':
+        game()
+
+
+
+
+    #JOURNEY BEGINS HERE
+
+    escape = True
+    
+    type ('You now hear voices in the distance, coming towards you.')
+    sleep(1)
+    type ('You quickly turn your head to the forest to find an escape.')
+    sleep(1)
+    type ('You see a waterfall that drops far below, a dense forest, and a steep uphill into snow and ice.')
+    while escape == True:
+        firstescape=input('What do you use to escape: 1. Waterfall, 2. Forest, 3. Uphill: ').lower()
+
+        if firstescape == 'waterfall':
+            type ('You decide that taking your chance in the water is the best choice. You sprint to the waterfall and take a dive......')
+            sleep(3)
+            type('After a long, wet, travel, you wind up in a shallow river. You check your surroundings to find a small village with a fisherman on a pier staring at you.')
+            type('You swim to him, and he pulls you out of the water.')
+            type('Good sir! Are you ok? I will give you some dry clothing. He then fetches you a new set of clothes.')
+            sleep(1)
+            type('You now own a sturdier set of clothes, which gives you a boost to attack')
+            escape = False
+        elif firstescape == 'forest':
+            type ('You decide that cover in the forest is the best option. You jump in the trees and run without being able see because of the trees.....')
+            escape = False
+        elif firstescape == 'uphill':
+            type ('You trust your legs and endurance in running up a hill. You begin up the hill into the icey cold......')
+            
+            escape = False
+        else:
+            escape = True
             
 #ADD REAL STATS HERE NEXT
 
